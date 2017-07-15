6962f06f889f66cfb87a8e9472e61da1fd2b95f5
PKxk8J <pkxk8j@users.noreply.github.com>
2017-07-15 21:47:11 +0900

    async await で書き直した

--- sort.js
+++ sort.js
@@ -17,6 +17,9 @@ const KEY_SORT = 'sort'
 const KEY_SORT_BY = 'sortBy'
 const KEY_SORTING = 'sorting'
 
+const KEY_SUCCESS_MESSAGE = 'successMessage'
+const KEY_FAILURE_MESSAGE = 'failureMessage'
+
 const NOTIFICATION_ID = i18n.getMessage(KEY_NAME)
 let notification = false
 
@@ -28,7 +31,7 @@ function debug (message) {
 }
 
 function onError (error) {
-  console.error('Error: ' + error)
+  console.error(error)
 }
 
 // bool が undefined でなく false のときだけ false になるように
@@ -56,67 +59,68 @@ function addMenuItem (id, title, parentId) {
 }
 
 // 右クリックメニューの変更
-function changeMenu (result) {
-  const flags = [
-    { key: KEY_URL, on: falseIffFalse(result[KEY_URL]) },
-    { key: KEY_URL_REV, on: result[KEY_URL_REV] },
-    { key: KEY_TITLE, on: falseIffFalse(result[KEY_TITLE]) },
-    { key: KEY_TITLE_REV, on: result[KEY_TITLE_REV] },
-    { key: KEY_RAND, on: result[KEY_RAND] }
-  ]
+async function changeMenu (result) {
+  const menuKeys = []
+
+  if (falseIffFalse(result[KEY_URL])) {
+    menuKeys.push(KEY_URL)
+  }
+  if (result[KEY_URL_REV]) {
+    menuKeys.push(KEY_URL_REV)
+  }
+  if (falseIffFalse(result[KEY_TITLE])) {
+    menuKeys.push(KEY_TITLE)
+  }
+  if (result[KEY_TITLE_REV]) {
+    menuKeys.push(KEY_TITLE_REV)
+  }
+  if (result[KEY_RAND]) {
+    menuKeys.push(KEY_RAND)
+  }
 
   // 一旦、全削除してから追加する
-  const removing = contextMenus.removeAll()
-  removing.then(() => {
-    debug('Clear menu items')
+  await contextMenus.removeAll()
+  debug('Clear menu items')
 
-    let count = 0
-    let sample
-    for (let flag of flags) {
-      if (flag.on) {
-        count++
-        sample = flag
-      }
+  switch (menuKeys.length) {
+    case 0: {
+      break
     }
-
-    switch (count) {
-      case 0: {
-        break
-      }
-      case 1: {
-        // 1 つだけのときはフラットメニュー
-        addMenuItem(sample.key, i18n.getMessage(KEY_SORT_BY, i18n.getMessage(sample.key)))
-        break
-      }
-      default: {
-        addMenuItem(KEY_SORT, i18n.getMessage(KEY_SORT))
-        for (let flag of flags) {
-          if (flag.on) {
-            addMenuItem(flag.key, i18n.getMessage(flag.key), KEY_SORT)
-          }
-        }
-      }
+    case 1: {
+      // 1 つだけのときはフラットメニュー
+      const key = menuKeys[0]
+      addMenuItem(key, i18n.getMessage(KEY_SORT_BY, i18n.getMessage(key)))
+      break
     }
-  }, onError)
+    default: {
+      addMenuItem(KEY_SORT, i18n.getMessage(KEY_SORT))
+      menuKeys.forEach((key) => addMenuItem(key, i18n.getMessage(key), KEY_SORT))
+    }
+  }
 }
 
 // 設定を反映させる
-function applySetting (result) {
+async function applySetting (result) {
+  debug('Apply ' + JSON.stringify(result))
   notification = result[KEY_NOTIFICATION]
-  changeMenu(result)
+  await changeMenu(result)
 }
 
 // リアルタイムで設定を反映させる
-const getting = storageArea.get()
-getting.then(applySetting, onError)
-storage.onChanged.addListener((changes, area) => {
+storage.onChanged.addListener((changes, area) => (async function () {
   const result = {}
   Object.keys(changes).forEach((key) => { result[key] = changes[key].newValue })
-  applySetting(result)
-})
+  await applySetting(result)
+})().catch(onError))
+
+// 初期化
+;(async function () {
+  const result = await storageArea.get()
+  await applySetting(result)
+})().catch(onError)
 
 // 並べ替える
-function rearrange (curOrder, idealOrder, callback) {
+async function rearrange (curOrder, idealOrder) {
   const idToIdealIndex = new Map()
   for (let i = 0; i < idealOrder.length; i++) {
     idToIdealIndex.set(idealOrder[i].id, i)
@@ -131,33 +135,19 @@ function rearrange (curOrder, idealOrder, callback) {
   let tailIndex = idealOrder.length - 1
   let curTailIndex = curOrder.length - 1
 
-  let nMoves = 0
+  let nMoved = 0
 
-  function onRearrangeError (error) {
-    onError(error)
-    const success = false
-    callback(success, idealOrder.length, nMoves)
-  }
-
-  function step () {
-    if (headIndex > tailIndex) {
-      const success = true
-      callback(success, idealOrder.length, nMoves)
-      return
-    }
-
+  while (headIndex <= tailIndex) {
     const curHeadId = curOrder[curHeadIndex].id
     if (orderedIds.has(curHeadId)) {
       curHeadIndex++
-      step()
-      return
+      continue
     }
 
     const curTailId = curOrder[curTailIndex].id
     if (orderedIds.has(curTailId)) {
       curTailIndex--
-      step()
-      return
+      continue
     }
 
     const idealHeadId = idealOrder[headIndex].id
@@ -165,8 +155,7 @@ function rearrange (curOrder, idealOrder, callback) {
       orderedIds.add(idealHeadId)
       headIndex++
       curHeadIndex++
-      step()
-      return
+      continue
     }
 
     const idealTailId = idealOrder[tailIndex].id
@@ -174,8 +163,7 @@ function rearrange (curOrder, idealOrder, callback) {
       orderedIds.add(idealTailId)
       tailIndex--
       curTailIndex--
-      step()
-      return
+      continue
     }
 
     // 既存の並びを利用できるまでに必要な挿入の回数
@@ -184,130 +172,98 @@ function rearrange (curOrder, idealOrder, callback) {
 
     if (headDiff <= tailDiff) {
       const index = headIndex
-      const moving = tabs.move(idealHeadId, {index})
-      moving.then(() => {
-        debug('Tab ' + idealHeadId + ' was moved to ' + index)
-        orderedIds.add(idealHeadId)
-        headIndex++
-        nMoves++
-        step()
-      }, onRearrangeError)
+      await tabs.move(idealHeadId, {index})
+      debug('Tab ' + idealHeadId + ' was moved to ' + index)
+      orderedIds.add(idealHeadId)
+      headIndex++
+      nMoved++
     } else {
       const index = tailIndex
-      const moving = tabs.move(idealTailId, {index})
-      moving.then(() => {
-        debug('Tab ' + idealTailId + ' was moved to ' + index)
-        orderedIds.add(idealTailId)
-        tailIndex--
-        nMoves++
-        step()
-      }, onRearrangeError)
+      await tabs.move(idealTailId, {index})
+      debug('Tab ' + idealTailId + ' was moved to ' + index)
+      orderedIds.add(idealTailId)
+      tailIndex--
+      nMoved++
     }
   }
 
-  step()
+  return nMoved
 }
 
-// タブのパラメータから順番を判定するキーを取り出す関数を受け取り、
-// タブをソートする関数をつくる
-function makeSorter (comparator) {
-  return (callback) => {
-    const querying = tabs.query({currentWindow: true})
-    querying.then((tabList) => {
-      // 現在の並び順
-      const curOrder = tabList.slice()
-      curOrder.sort((tab1, tab2) => tab1.index - tab2.index)
+// タブをソートする
+async function sort (comparator) {
+  const tabList = await tabs.query({currentWindow: true})
 
-      let firstUnpinnedIndex = 0
-      for (; firstUnpinnedIndex < curOrder.length; firstUnpinnedIndex++) {
-        if (!curOrder[firstUnpinnedIndex].pinned) {
-          break
-        }
-      }
+  // 現在の並び順
+  tabList.sort((tab1, tab2) => tab1.index - tab2.index)
 
-      // ソート後の並び順
-      const unpinnedIdealOrder = curOrder.slice(firstUnpinnedIndex)
-      unpinnedIdealOrder.sort(comparator)
-      const idealOrder = curOrder.slice(0, firstUnpinnedIndex).concat(unpinnedIdealOrder)
+  let firstUnpinnedIndex = 0
+  for (; firstUnpinnedIndex < tabList.length; firstUnpinnedIndex++) {
+    if (!tabList[firstUnpinnedIndex].pinned) {
+      break
+    }
+  }
 
-      rearrange(curOrder, idealOrder, callback)
-      // // 以下のコードはタブが多いと固まる場合がある
-      // const idealIds = idealOrder.map((tab) => tab.id)
-      // const moving = tabs.move(idealIds, {index: 0})
-      // moving.then(() => {
-      //   const success = true
-      //   callback(success, idealOrder.length, -1)
-      // }, (error) => {
-      //   onError(error)
-      //   const success = false
-      //   callback(success, idealOrder.length, -1)
-      // })
-    }, (error) => {
-      onError(error)
-      const success = false
-      callback(success, -1, -1)
-    })
+  // ソート後の並び順
+  const unpinnedIdealOrder = tabList.slice(firstUnpinnedIndex)
+  unpinnedIdealOrder.sort(comparator)
+  const idealOrder = tabList.slice(0, firstUnpinnedIndex).concat(unpinnedIdealOrder)
+
+  const nMoved = await rearrange(tabList, idealOrder)
+  return {
+    all: tabList.length,
+    moved: nMoved
   }
 }
 
-function getResultMessage (success, seconds, nTabs, nMoveTabs) {
-  const key = (success ? 'successMessage' : 'failureMessage')
-  return i18n.getMessage(key, [seconds, nTabs, nMoveTabs])
-}
-
-function sort (comparator) {
-  const start = new Date()
-
-  if (!notification) {
-    makeSorter(comparator)((success, nTabs, nMoveTabs) => {
-      const seconds = (new Date() - start) / 1000
-      const message = getResultMessage(success, seconds, nTabs, nMoveTabs)
-      debug(message)
-    })
-    return
-  }
-
-  const creatingStart = notifications.create(NOTIFICATION_ID, {
+// 通知を表示する
+async function notify (message) {
+  await notifications.create(NOTIFICATION_ID, {
     'type': 'basic',
     'title': NOTIFICATION_ID,
-    message: i18n.getMessage(KEY_SORTING)
+    message: message
   })
-  creatingStart.then(() => {
-    makeSorter(comparator)((success, nTabs, nMoveTabs) => {
-      const seconds = (new Date() - start) / 1000
-      const message = getResultMessage(success, seconds, nTabs, nMoveTabs)
-      debug(message)
-      const creatingEnd = notifications.create(NOTIFICATION_ID, {
-        'type': 'basic',
-        'title': NOTIFICATION_ID,
-        message
-      })
-      creatingEnd.then(() => debug('End'), onError)
-    })
-  }, onError)
 }
 
-contextMenus.onClicked.addListener((info, tab) => {
+// 前後処理で挟む
+async function wrapSort (comparator) {
+  if (notification) {
+    await notify(i18n.getMessage(KEY_SORTING))
+  }
+
+  const start = new Date()
+  const {all, moved} = await sort(comparator)
+  const seconds = (new Date() - start) / 1000
+  const message = i18n.getMessage(KEY_SUCCESS_MESSAGE, [seconds, all, moved])
+
+  debug(message)
+  if (notification) {
+    await notify(message)
+  }
+}
+
+// 右クリックメニューからの入力を処理
+contextMenus.onClicked.addListener((info, tab) => (async function () {
   switch (info.menuItemId) {
     case KEY_URL: {
-      sort((tab1, tab2) => tab1.url.localeCompare(tab2.url))
+      await wrapSort((tab1, tab2) => tab1.url.localeCompare(tab2.url))
       break
     }
     case KEY_URL_REV: {
-      sort((tab1, tab2) => -tab1.url.localeCompare(tab2.url))
+      await wrapSort((tab1, tab2) => -tab1.url.localeCompare(tab2.url))
       break
     }
     case KEY_TITLE: {
-      sort((tab1, tab2) => tab1.title.localeCompare(tab2.title))
+      await wrapSort((tab1, tab2) => tab1.title.localeCompare(tab2.title))
       break
     }
     case KEY_TITLE_REV: {
-      sort((tab1, tab2) => -tab1.title.localeCompare(tab2.title))
+      await wrapSort((tab1, tab2) => -tab1.title.localeCompare(tab2.title))
       break
     }
     case KEY_RAND: {
       const random = []
-      sort((tab1, tab2) => {
+      await wrapSort((tab1, tab2) => {
         const index = Math.max(tab1.index, tab2.index)
         while (random.length <= index) {
           random.push(Math.random())
@@ -317,4 +273,9 @@ contextMenus.onClicked.addListener((info, tab) => {
       break
     }
   }
-})
+})().catch((e) => {
+  onError(e)
+  if (notification) {
+    notify(i18n.getMessage(KEY_FAILURE_MESSAGE, e)).catch(onError)
+  }
+}))
